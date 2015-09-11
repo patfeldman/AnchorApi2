85e3425a98cf14b8c02f274d4f12922e4cead26f
patfeldman <patfeldman@yahoo.com>
2015-09-11 06:21:54 -0400

    updated database

--- database/anchornet.sql
+++ database/anchornet.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.3.11
+-- version 4.0.9
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 04, 2015 at 10:15 PM
--- Server version: 5.6.24
--- PHP Version: 5.6.8
+-- Generation Time: Sep 11, 2015 at 12:20 PM
+-- Server version: 5.5.34
+-- PHP Version: 5.4.22
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -27,13 +27,22 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `authaccess` (
-  `accessId` int(11) NOT NULL,
+  `accessId` int(11) NOT NULL AUTO_INCREMENT,
   `authToken` varchar(25) NOT NULL,
   `date` datetime NOT NULL,
   `userId` int(11) NOT NULL,
   `ipAddress` varchar(30) NOT NULL,
-  `successLogin` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `successLogin` tinyint(1) NOT NULL,
+  PRIMARY KEY (`accessId`),
+  KEY `User` (`userId`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
+
+--
+-- Dumping data for table `authaccess`
+--
+
+INSERT INTO `authaccess` (`accessId`, `authToken`, `date`, `userId`, `ipAddress`, `successLogin`) VALUES
+(14, '8JeXyK8cvkpiljb4yccm', '2015-09-11 09:46:37', 14, '', 0);
 
 -- --------------------------------------------------------
 
@@ -42,11 +51,12 @@ CREATE TABLE IF NOT EXISTS `authaccess` (
 --
 
 CREATE TABLE IF NOT EXISTS `crew` (
-  `crewId` int(11) NOT NULL,
+  `crewId` int(11) NOT NULL AUTO_INCREMENT,
   `crewName` varchar(50) DEFAULT NULL,
   `numPeople` int(11) NOT NULL,
-  `crewDescription` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `crewDescription` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`crewId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -55,12 +65,13 @@ CREATE TABLE IF NOT EXISTS `crew` (
 --
 
 CREATE TABLE IF NOT EXISTS `crewchatmessage` (
-  `messageId` int(11) NOT NULL,
+  `messageId` int(11) NOT NULL AUTO_INCREMENT,
   `crewId` int(11) NOT NULL,
   `fromUid` int(11) NOT NULL,
   `date` datetime NOT NULL,
-  `message` tinyblob NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `message` tinyblob NOT NULL,
+  PRIMARY KEY (`messageId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -69,11 +80,14 @@ CREATE TABLE IF NOT EXISTS `crewchatmessage` (
 --
 
 CREATE TABLE IF NOT EXISTS `crewmember` (
-  `crewMemberId` int(11) NOT NULL,
+  `crewMemberId` int(11) NOT NULL AUTO_INCREMENT,
   `groupId` int(11) NOT NULL,
   `userId` int(11) NOT NULL,
-  `isAdmin` tinyint(1) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `isAdmin` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`crewMemberId`),
+  KEY `group` (`groupId`),
+  KEY `user` (`userId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -82,12 +96,13 @@ CREATE TABLE IF NOT EXISTS `crewmember` (
 --
 
 CREATE TABLE IF NOT EXISTS `moodhistory` (
-  `historyId` int(11) NOT NULL,
+  `historyId` int(11) NOT NULL AUTO_INCREMENT,
   `userId` int(11) NOT NULL,
   `moodGroup` int(11) NOT NULL,
   `moodItem` int(11) NOT NULL,
-  `date` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `date` datetime NOT NULL,
+  PRIMARY KEY (`historyId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -96,12 +111,15 @@ CREATE TABLE IF NOT EXISTS `moodhistory` (
 --
 
 CREATE TABLE IF NOT EXISTS `privatechatmessage` (
-  `messageId` int(11) NOT NULL,
+  `messageId` int(11) NOT NULL AUTO_INCREMENT,
   `user1Id` int(11) NOT NULL,
   `user2Id` int(11) NOT NULL,
   `date` datetime NOT NULL,
-  `message` tinyblob NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `message` tinyblob NOT NULL,
+  PRIMARY KEY (`messageId`),
+  KEY `user1` (`user1Id`),
+  KEY `User2` (`user2Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -110,7 +128,7 @@ CREATE TABLE IF NOT EXISTS `privatechatmessage` (
 --
 
 CREATE TABLE IF NOT EXISTS `user` (
-  `userid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(60) NOT NULL,
   `password` varchar(50) NOT NULL,
   `temppassword` varchar(50) NOT NULL,
@@ -118,94 +136,20 @@ CREATE TABLE IF NOT EXISTS `user` (
   `avatarType` int(11) NOT NULL DEFAULT '0',
   `current_latitude` float NOT NULL,
   `current_longitude` float NOT NULL,
-  `creation_date` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `creation_date` datetime NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `phone` varchar(13) NOT NULL,
+  PRIMARY KEY (`userid`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
 
 --
--- Indexes for dumped tables
+-- Dumping data for table `user`
 --
 
---
--- Indexes for table `authaccess`
---
-ALTER TABLE `authaccess`
-  ADD PRIMARY KEY (`accessId`), ADD KEY `User` (`userId`);
+INSERT INTO `user` (`userid`, `username`, `password`, `temppassword`, `verified`, `avatarType`, `current_latitude`, `current_longitude`, `creation_date`, `email`, `phone`) VALUES
+(14, 'pat', 'feldman', '', 0, 0, 0, 0, '0000-00-00 00:00:00', 'patfeldman@yahoo.com', '1234567890');
 
---
--- Indexes for table `crew`
---
-ALTER TABLE `crew`
-  ADD PRIMARY KEY (`crewId`);
-
---
--- Indexes for table `crewchatmessage`
---
-ALTER TABLE `crewchatmessage`
-  ADD PRIMARY KEY (`messageId`);
-
---
--- Indexes for table `crewmember`
---
-ALTER TABLE `crewmember`
-  ADD PRIMARY KEY (`crewMemberId`), ADD KEY `group` (`groupId`), ADD KEY `user` (`userId`);
-
---
--- Indexes for table `moodhistory`
---
-ALTER TABLE `moodhistory`
-  ADD PRIMARY KEY (`historyId`);
-
---
--- Indexes for table `privatechatmessage`
---
-ALTER TABLE `privatechatmessage`
-  ADD PRIMARY KEY (`messageId`), ADD KEY `user1` (`user1Id`), ADD KEY `User2` (`user2Id`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`userid`), ADD UNIQUE KEY `username` (`username`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `authaccess`
---
-ALTER TABLE `authaccess`
-  MODIFY `accessId` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `crew`
---
-ALTER TABLE `crew`
-  MODIFY `crewId` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `crewchatmessage`
---
-ALTER TABLE `crewchatmessage`
-  MODIFY `messageId` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `crewmember`
---
-ALTER TABLE `crewmember`
-  MODIFY `crewMemberId` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `moodhistory`
---
-ALTER TABLE `moodhistory`
-  MODIFY `historyId` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `privatechatmessage`
---
-ALTER TABLE `privatechatmessage`
-  MODIFY `messageId` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
